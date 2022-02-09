ab61899496455f4916034563fccc807b1b63668a
Maxim Nemchenko <marsel.baym@mail.ru>
2022-02-09 15:44:53 +0300

    Запилил, но качественно надо еще тестами покрыть.

--- file_test.cpp
+++ file_test.cpp
@@ -66,9 +66,9 @@ void FileTest::PutInfoOK_FALSE(int currentTest, int ok_or_false)
 {
     if(ok_or_false == 1)
     {
-        ui->tbInfo->append("Тест " + QString::number(currentTest+1) + " выполнен успешно ");
+        ui->tbInfo->append("Тест " + QString::number(currentTest+1) + "<font color='green'> выполнен успешно");
     }else {
-        ui->tbInfo->append("Тест " + QString::number(currentTest+1) + " не выполнен ");
+        ui->tbInfo->append("Тест " + QString::number(currentTest+1) + "<font color='red'> не выполнен ");
     }
 }
 
@@ -126,7 +126,7 @@ void FileTest::MainTest()
 
         case file_test::TestOpuState::TEST_FALSE:
         {
-            PutInfoOK_FALSE(currentTest, 1);
+            PutInfoOK_FALSE(currentTest, 0);
             currentTest++;
             falseTest++;
 
@@ -183,13 +183,6 @@ void FileTest::on_bOpenFile_clicked()
     ui->lSuccess->setText(QString::number(0));
 
     ui->tbInfo->clear();
-
-
-//    QVector<file_test::TestOpu>::iterator it = tests.begin(); // создаем итератор и переводим его в начало списка
-//    while (it != tests.end()) {
-//         qDebug()  << it->az << it->el << it->pol;
-//         ++it;
-//    }
 
     if(tests.size() > 0)
         ui->bStart->setEnabled(true);
@@ -260,245 +253,6 @@ bool FileTest::checkValues(file_test::TestOpu test)
 
     return true;
 }
-
-//void FileTest::newStatus(const ca_messages::out::Status &status)
-//{
-//    if(!isVisible())
-//        return;
-
-//    if(!startFlag)
-//        return;
-
-////    qDebug() << "status" << status.opu_az_state_drive << status.opu_el_state_drive << status.position.azimuth << status.position.elevation;
-
-//    position.azimuth = status.position.azimuth;
-//    position.elevation = status.position.elevation;
-//    position.polarization = status.position.polarization;
-
-//    if(state == file_test::TestOpuState::WAIT_STOP_STATUS && status.opu_az_state_drive == 0 && status.opu_el_state_drive == 0){
-////        qDebug() << "стоим";
-
-//        if(fabs(position.azimuth - tests[currentTest].az) <= errorAngle &&
-//                fabs(position.elevation - tests[currentTest].el) <= errorAngle &&
-//                (status.device == ca_messages::Device::NAP_KU ?
-//                 (fabs(position.polarization - tests[currentTest].pol) <= errorAngle) : true)){
-
-
-//            updateResult(currentTest, true);
-
-//            ca_messages::in::StopMove command;
-//            ca_messages::Message message;
-//            message.append(reinterpret_cast<char*>(&command), ca_messages::sizeOfStopMove);
-//            emit messageReady(message);
-//            state = file_test::TestOpuState::WAIT_STOP_MOVE;
-
-//            currentTest++;
-
-//        } else {
-//             runTest(currentTest);
-//        }
-
-//        return;
-//    }
-
-//    if(state == file_test::TestOpuState::WAIT_STOP_STATUS) {
-////        qDebug() << "запрашиваем новый статус";
-
-////        statusTimer.start(500);
-//        return;
-//    }
-
-//    if(state == file_test::TestOpuState::WAIT_START_TEST && status.opu_az_state_drive == 0 && status.opu_el_state_drive == 0) {
-////        qDebug() << status.opu_az_state_drive << status.opu_el_state_drive;
-//        return;
-//    }
-
-//    if(state == file_test::TestOpuState::WAIT_START_TEST && (status.opu_az_state_drive == 1 || status.opu_el_state_drive == 1)) {
-
-////        qDebug() << "начинаем тест";
-
-//        waitstartTimer.stop();
-
-//        uint dAz = ceil(fabs(position.azimuth - tests[currentTest].az));
-//        uint dEl = ceil(fabs(position.elevation - tests[currentTest].el));
-//        uint dPol = ceil(fabs(position.polarization - tests[currentTest].pol));
-
-//        uint maxValue = std::max(std::max(dAz, dEl), dPol);
-
-////        qDebug() << "waittime" <<maxValue;
-//        testTimer.stop();
-//        testTimer.start(maxValue * 1000 + waittime);
-
-//        state = file_test::TestOpuState::RUN_TEST;
-
-//        return;
-//    }
-
-
-
-//    if(state == file_test::TestOpuState::RUN_TEST && status.opu_az_state_drive == 0 && status.opu_el_state_drive == 0) {
-//        checkPosition = true;
-//    }
-
-
-//    if(state == file_test::TestOpuState::RUN_TEST && checkPosition == true){
-
-//        float diff = position.azimuth - tests[currentTest].az;
-//        if(diff > 359)
-//            diff-=360;
-//        else if(diff < -359)
-//            diff+=360;
-
-//        if(status.opu_az_state_drive != 0){
-//            updateResult(currentTest, false);
-//            ui->tbInfo->append("opu_az_state_drive:" + QString::number(status.opu_az_state_drive));
-//        }
-//        else if(status.opu_el_state_drive != 0){
-//            updateResult(currentTest, false);
-//            ui->tbInfo->append("opu_az_state_drive:" + QString::number(status.opu_el_state_drive));
-//        }
-//        else if(fabs(diff) > errorAngle)
-//        {    updateResult(currentTest, false);
-//        ui->tbInfo->append("current az:" + QString::number(position.azimuth) +
-//                           " az:" + QString::number(tests[currentTest].az));
-//        }
-//        else if(fabs(position.elevation - tests[currentTest].el) > errorAngle){
-//            updateResult(currentTest, false);
-//            ui->tbInfo->append("current el:" + QString::number(position.elevation) +
-//                               " el:" + QString::number(tests[currentTest].el));
-//        }
-//        else if(status.device == ca_messages::Device::NAP_KU && fabs(position.polarization - tests[currentTest].pol) > errorAngle)
-//        {
-//            updateResult(currentTest, false);
-//            ui->tbInfo->append("current pol:" + QString::number(position.polarization) +
-//                               " pol:" + QString::number(tests[currentTest].pol));
-//        } else {
-//            updateResult(currentTest, true);
-//        }
-//        currentTest++;
-
-////        QThread::msleep(500);
-
-////qDebug() << "stop move" << status.opu_az_state_drive << status.opu_el_state_drive;
-//        ca_messages::in::StopMove command;
-//        ca_messages::Message message;
-//        message.append(reinterpret_cast<char*>(&command), ca_messages::sizeOfStopMove);
-//        emit messageReady(message);
-//        state = file_test::TestOpuState::WAIT_STOP_MOVE;
-
-//        checkPosition = false;
-//        testTimer.stop();
-//    }
-
-//    if(currentTest == static_cast<uint>(tests.size()))
-//    {
-//        restart();
-//    }
-
-
-//}
-
-//void FileTest::newOk(const ca_messages::out::Ok &ok)
-//{
-//    if(!isVisible())
-//        return;
-
-//    if(ok.command == ca_messages::CMD_CHECK_LINK)
-//        return;
-
-//    if(ok.command == ca_messages::CMD_STOP_MOVE && state == file_test::TestOpuState::WAIT_STOP_MOVE)
-//        state = file_test::TestOpuState::WAIT_STOP_STATUS;
-
-//    if(ok.command == ca_messages::CMD_ROTATE_MANUAL)// && state == TestOpuState::WAIT_START_TEST)
-//    {
-//        state = file_test::TestOpuState::WAIT_START_TEST;
-////        qDebug() << "state" << (uint8_t)state;
-//        waitstartTimer.start(waitStartTime*1000);
-//    }
-
-////    qDebug() << "ok"<< ok.command ;
-//}
-
-//void FileTest::newError(const ca_messages::out::Error &error)
-//{
-//    if(!isVisible())
-//        return;
-
-//    if(error.command == ca_messages::CMD_CHECK_LINK)
-//        return;
-
-//    restart();
-//    errorTest();
-//}
-
-//void FileTest::errorTest()
-//{
-//    ui->tbInfo->setStyleSheet("background-color: rgb(255, 0, 0);");
-//    ui->tbInfo->append("Ошибка выполнения теста");
-//}
-
-//void FileTest::restart()
-//{
-
-//    startFlag = false;
-//    checkPosition = false;
-
-//    successTest = 0;
-
-//    ui->bStop->setEnabled(false);
-//    ui->bStart->setEnabled(true);
-//    ui->bOpenFile->setEnabled(true);
-
-//    currentTest = 0;
-
-//    statusTimer.stop();
-//    waitstartTimer.stop();
-
-//}
-
-//void FileTest::updateResult(uint test, bool result)
-//{
-//    if(test == static_cast<uint>(tests.size()))
-//        return;
-
-//    if(result){
-//        ui->tbInfo->append("Тест " + QString::number(test+1) + " выполнен успешно");
-//        successTest++;
-//        ui->lSuccess->setText(QString::number(successTest));
-//    }
-//    else
-//        ui->tbInfo->append("Тест " + QString::number(test+1) + " не выполнен");
-//}
-
-//void FileTest::runTest(uint test)
-//{
-
-//    testTimer.stop();
-//    if(test >= static_cast<uint>(tests.size()))
-//    {
-//        state = file_test::TestOpuState::FINISH_TEST;
-//        restart();
-//        return;
-//    }
-
-//    ui->tbInfo->append("Тест " + QString::number(test+1) + " выполняется...");
-//    ui->tbInfo->append("Азимут: " + QString::number(tests[test].az) + " Угол места: "
-//                       + QString::number(tests[test].el) + " Поляризация: "
-//                       + QString::number(tests[test].pol));
-
-//    ca_messages::in::RotateManual command;
-//    command.az = tests[test].az;
-//    command.el = tests[test].el;
-//    command.pol = tests[test].pol;
-//    ca_messages::Message message;
-//    message.append(reinterpret_cast<char*>(&command), ca_messages::sizeOfRotateManual);
-//    emit messageReady(message);
-
-////    qDebug() << "rotate manual:" << command.az << command.el << command.pol;
-////    state = TestOpuState::WAIT_START_TEST;
-//}
-
-
 
 void FileTest::on_bStart_clicked()
 {
--- file_test.h
+++ file_test.h
@@ -48,20 +48,13 @@ public:
 
 public slots:
     void GetStatusFotTesting(QByteArray *AnsData);
-    //void newStatus(const ca_messages::out::Status& status);
-    //void newOk(const ca_messages::out::Ok &ok);
-    //void newError(const ca_messages::out::Error &error);
 
 private slots:
     void on_bOpenFile_clicked();
     void MainTest();
     void OneTest();
-    //void on_bStart_clicked();
-    //void on_bStop_clicked();
-
     void on_bStart_clicked();
     void read_status();
-
     void on_bStop_clicked();
 
 signals:
@@ -92,27 +85,21 @@ private:
     static constexpr float minPol = -180.;
     static constexpr float maxPol = 180.;
 
-    bool startFlag = false;
-    //ca_messages::out::Position position;
-    //file_test::TestOpuState state;
-
-
     constexpr static float errorAngle = 0.3;
-    constexpr static uint waittime = 500;
-    constexpr static uint waitStartTime = 10;
 
     bool checkPosition = false;
 
     QTimer waitstartTimer;
     QTimer testTimer;
-
-    void errorTest();
-    void restart();
-    void updateResult(uint test, bool result);
-    void runTest(uint test);
-
-
-
 };
 
 #endif // CA_FILETESTOPUDIALOG_H
+
+
+
+
+
+
+
+
+
